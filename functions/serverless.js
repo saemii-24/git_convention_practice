2e05dd24c9b27d114f8359d5b26cc99e5ef7f9c3
saemii-24 <hims2419@gmail.com>
2023-11-24 08:03:04 +0900

    serverless function

---
+++ functions/serverless.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+exports.handler = async function (event, context) {
+  const apiKey = process.env.API_KEY;
+  const url = "/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList";
+  axios
+    .get(url, {
+      params: {
+        serviceKey: apiKey,
+        numOfRows: 1,
+        pageNo: 10,
+      },
+    })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
