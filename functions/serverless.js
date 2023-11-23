b88d5742e064b5c4793158ec3aa4eaf03809c2d6
saemii-24 <hims2419@gmail.com>
2023-11-24 08:22:02 +0900

    serverless return 추가

--- functions/serverless.js
+++ functions/serverless.js
@@ -1,21 +1,34 @@
-import axios from "axios";
+const axios = require("axios");
 
 exports.handler = async function (event, context) {
   const apiKey = process.env.API_KEY;
-  const url =
+  const apiUrl =
     "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList";
-  axios
-    .get(url, {
+
+  try {
+    const response = await axios.get(apiUrl, {
       params: {
         serviceKey: apiKey,
         numOfRows: 1,
         pageNo: 10,
       },
-    })
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
     });
+
+    const xmlData = response.data;
+
+    return {
+      statusCode: 200,
+      body: xmlData,
+      headers: {
+        "Content-Type": "application/xml",
+      },
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "에러발생" }),
+    };
+  }
 };
--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 
 function App() {
-  const response = axios.get("/.netlify/functions/serverless");
-  console.log(response.data);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/.netlify/functions/serverless");
+      const data = response.data;
 
+      console.log(data);
+    } catch (error) {
+      console.error("에러 발생: " + error);
+    }
+  };
+  fetchData();
   return <div className="App">야호!</div>;
 }
 
